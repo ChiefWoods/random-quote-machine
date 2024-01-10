ab643fef9ae656009fe3d09f463181f40b30e8e1
Chii Yuen <chii.yuen@hotmail.com>
2024-01-11 02:24:33 +0800

    Add LogoBtn.svelte

---
+++ src/lib/LogoBtn.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let id: string;
+  export let icon: string;
+  export let href: string;
+  export let alt: string;
+</script>
+
+<a {id} {href} target="_blank">
+  <img src={icon} {alt} />
+</a>
+
+<style>
+  a,
+  :global(#new-quote) {
+    background: var(--current-color);
+    border-radius: 3px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+  }
+
+  a:hover,
+  :global(#new-quote):hover {
+    opacity: 0.8;
+  }
+
+  img {
+    width: 20px;
+    aspect-ratio: 1/1;
+  }
+</style>
